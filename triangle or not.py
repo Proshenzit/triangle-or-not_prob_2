1021ff67aa6b4bd159a39164e0d3ca1702b42a95
Proshenzit Roy <37993972+Proshenzit@users.noreply.github.com>
2024-01-07 21:25:49 -0800

    Add files via upload

---
+++ triangle or not.py
@@ -0,0 +1,7 @@
+a  = int(input("enter your  first number: "))
+b  = int(input("enter your  first number: "))
+c  = int(input("enter your  first number: "))
+if a+b+c==180 and a!=0 and b!=0 and c!=0:
+    print('Not possible:')
+else:
+        print("possible:")
